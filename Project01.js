fb7d04968e45f0474ad77c1f004f674d2c89409e
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2023-06-17 11:01:27 +0530

    Project01.js
    This is Java Script Code Files

---
+++ Project01.js
@@ -0,0 +1,15 @@
+// Add Style using JAVA Script
+
+
+    // target the elements likes : - button and paragraph
+    const element1 = document.getElementById("onePara");
+    const btn1 = document.getElementById("fristbtn");
+
+    function addStyle(){
+        element1.style.fontSize = "20px";
+        element1.style.fontWeight = "700";
+        element1.style.color = "green";
+
+    }
+
+
